5f024bb33452c329d898dbc54e1917bbc37088dd
Peter Kim <peterckim96@gmail.com>
2018-06-04 08:03:23 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^\b[A-Z]\b + \b\b* + [.!?;:]$/) != nil
+  if text.match(/^[A-Z]/) != nil
     return true
   else
     return false

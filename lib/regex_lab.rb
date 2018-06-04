1d564558da1b13575fde50d47de07c2b1e6d17e6
Peter Kim <peterckim96@gmail.com>
2018-06-04 08:04:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(//) != nil
+  if phone.match(/(/) != nil
     return true
   else
     return false
